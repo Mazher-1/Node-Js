21f626fd5c1e957244a9c712366bd0b3a31461aa
MH Code <mazheriftikhar1@gmail.com>
2024-10-30 15:02:26 +0500

    30-oct changes

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const a = require('./mymod.js');
+const express = require('express')
+const app = express()
 
-console.log(a(2, 4));
+
+app.get('/', (req,res) =>{
+    res.send('Welcome to Homepage')
+});
+
+app.get('/about', (req , res) =>{
+    res.send("Welcome to About Page")
+});
+
+
+const PORT = 3000;
+
+app.listen(PORT, () =>{
+    console.log(`Server running: ${PORT}`)
+})
